4d8c7ca7ed4b1d9a8f72d7cce7f85b7188880361
Danny Santoro <danielrsantoro@gmail.com>
2017-05-10 11:39:33 -0400

    1.1.0

--- readme.txt
+++ readme.txt
@@ -1,8 +1,8 @@
 === WC Password Strength Settings ===
-Contributors: danielsantoro
+Contributors: danielsantoro, rynald0s
 Tags: WooCommerce, Users, Accounts, Passwords, Security
 Requires at least: 4.5.0
-Tested up to: 4.5.3
+Tested up to: 4.7.3
 Stable tag: trunk
 License: GPL
 License URI: http://www.gnu.org/licenses/gpl.html
@@ -15,17 +15,28 @@ Allows administrators to set minimum password strength requirements for their Wo
 
 Introduced in recent versions of WooCommerce, there is an integrated Password Strength Meter which forces users to use strong passwords. Sometimes this isn't desirable - with this plugin, you can choose between five password levels ranging from "Anything Goes" to "Strong Passwords Only".
 
+= What's New? =
+
+As of plugin version 1.1.0, you can change the messaging that appears as a user is typing through custom fields. Special thanks to Rynaldo for making this possible!
 
 = Notes =
 
-This plugin was created and tested using WordPress 4.5.3. No guarantee provided - please use the plugin with caution. If you experience any issues, please [let the developer know](https://github.com/DanielSantoro/wc-password-strength-settings).
+If you experience any issues or have ideas for features, please [let the developer know](https://github.com/DanielSantoro/wc-password-strength-settings).
 
 While this does allow for user accounts to have weaker passwords, it's a good idea to still encourage strong password use - _especially_ for administrators!
+
+= Planned Features =
+
+The below list is things that are on the eventual roadmap:
+
+* Ability to remove smiley faces
+* Ability to change color of messaging
+* Ability to optionally remove the password guidelines
 
 == Installation ==
 1. Download the plugin & install it to your `wp-content/plugins` folder (or use the Plugins menu through the WordPress Administration section)
 2. Activate the plugin
-3. Navigate to **WooCommerce > Settings > Accounts** and edit the field at the bottom.
+3. Navigate to **WooCommerce > Settings > Accounts** and edit the fields at the bottom. There, you can choose the strength of the required passwords as well as change the messaging that appears as a user enters their password.
 4. Save and enjoy!
 
 == Frequently Asked Questions ==
@@ -35,13 +46,23 @@ A: The five levels range from 1 (lowest) to 5 (highest). As passwords are typed,
 = Q: Where does this show up? =
 A: This should appear wherever the Password Strength Meter appears - in the "My Account" page or during Checkout.
 
-= Q: Where can I go if I find an issue? =
+= Q: Where can I go if I find an issue or want to recommend a feature? =
 A: You can submit a new issue on the [Public GitHub Repository](https://github.com/DanielSantoro/wc-password-strength-settings).
 
 = Q: Dang, this is pretty awesome. Where can I see some of your other stuff? =
-A: Check out the developer's personal site at [DanielSantoro.com](http://danielsantoro.com).
+A: Two places! You can check out the Danny's personal site at [DanielSantoro.com](https://danielsantoro.com), or follow Rynaldo on his [WordPress.org page](https://profiles.wordpress.org/rynald0s/).
 
 == Changelog ==
+
+= 4/5/2017 - Version 1.1.0 =
+ * Added fields to allow for custom messaging as a user is inputting passwords.
+ * Added rynald0s as a co-author, because he's a modern-day superhero.
+
+= 3/28/2017 - Version 1.0.2 =
+ * Readme fixes, added setting to change password strength meter labels / password error message
+
+= 9/28/2016 - Version 1.0.1 =
+ * Readme fixes, version check to WordPress 4.6 compatibility
 
 = 7/24/2016 - Version 1.0.0 =  
  * Initial Release
--- wc-password-strength-settings.php
+++ wc-password-strength-settings.php
@@ -4,21 +4,21 @@
  Plugin URI: https://github.com/DanielSantoro/wc-password-strength-settings
  Description: Allows administrators to set the required password strength or disable it entirely from the WooCommerce Accounts menu.
  Author: Daniel Santoro
- Author URI: http://danielsantoro.com
- Version: 1.0.0
+ Author URI: https://danielsantoro.com
+ Version: 1.1.0
  License: GPLv2 or later
  */
-add_filter('woocommerce_get_settings_account','ds_woo_account_setting');
-function ds_woo_account_setting($settings){
+add_filter( 'woocommerce_get_settings_account','ds_woo_account_setting' );
+function ds_woo_account_setting($settings) {
     $settings[]=array( 'title' => __( 'User Password Strength Settings', 'woocommerce' ), 'type' => 'title', 'id' => 'account_password_options' );
     $settings[]=array(
                 'title'    => __( 'Strength Requirement', 'woocommerce' ),
-                'desc'     => __( 'Select the minimum strength of your user passwords. Level 4 = Default.', 'woocommerce' ) ,
+                'desc'     => __( 'Select the minimum strength of your user passwords. Level 4 = Default.', 'woocommerce' ),
                 'id'       => 'woocommerce_myaccount_password_strength',
                 'type'     => 'select',
                 'default'  => '',
                 'class'    => 'wc-enhanced-select',
-                'css'      => 'min-width:300px;',
+                'css'      => 'min-width:350px;',
                 'desc_tip' => true,
                 'options'  =>array(
                     '0'=>__( 'Level 1 - Anything', 'woocommerce' ),
@@ -28,12 +28,93 @@ function ds_woo_account_setting($settings){
                     '4'=>__( 'Level 5 - Strong', 'woocommerce' ),
                 ),
             );
+
+    $settings[]=array(
+				'title'    => __( 'Level 1 Message', 'woocommerce' ),
+				'desc'     => __( 'Usually is not displayed - can be left as-is.', 'woocommerce' ),
+				'id'       => 'woocommerce_password_strength_label_1',
+				'type'     => 'text',
+				'default'  => 'empty',
+				'placeholder' => 'empty',
+				'desc_tip' => true,
+			);
+
+    $settings[]=array(
+				'title'    => __( 'Level 2 Message', 'woocommerce' ),
+				'desc'     => __( 'Will display until level 2 strength bypassed.', 'woocommerce' ),
+				'id'       => 'woocommerce_password_strength_label_2',
+				'type'     => 'text',
+				'default'  => 'Short: Your password is too short.',
+				'placeholder'  => 'Your password is too short.',
+				'desc_tip' => true,
+			);
+
+    $settings[]=array(
+				'title'    => __( 'Level 3 Message', 'woocommerce' ),
+				'desc'     => __( 'Will display until level 3 strength bypassed.', 'woocommerce' ),
+				'id'       => 'woocommerce_password_strength_label_3',
+				'type'     => 'text',
+				'default'  => 'Password Strength: Weak',
+				'placeholder'  => 'Password Strength: Weak',
+				'desc_tip' => true,
+			);
+
+    $settings[]=array(
+				'title'    => __( 'Level 4 Message', 'woocommerce' ),
+				'desc'     => __( 'Will display until level 4 strength bypassed.', 'woocommerce' ),
+				'id'       => 'woocommerce_password_strength_label_4',
+				'type'     => 'text',
+				'default'  => 'Password Strength: OK',
+				'placeholder'  => 'Password Strength: OK',
+				'desc_tip' => true,
+			);
+
+    $settings[]=array(
+				'title'    => __( 'Level 5 Message', 'woocommerce' ),
+				'desc'     => __( 'Will display until level 5 strength bypassed.', 'woocommerce' ),
+				'id'       => 'woocommerce_password_strength_label_5',
+				'type'     => 'text',
+				'default'  => 'Password Strength: Strong',
+				'placeholder'  => 'Password Strength: Strong',
+				'desc_tip' => true,
+			);
+
+        $settings[]=array(
+				'title'    => __( 'Error Message', 'woocommerce' ),
+				'desc'     => __( 'Will display if there is an error in the user-selected password.', 'woocommerce' ),
+				'id'       => 'woocommerce_password_error',
+				'type'     => 'text',
+				'default'  => 'Please try again.',
+				'placeholder'  => 'Please try again.',
+				'desc_tip'  => true,
+			);
+
     $settings[]=array( 'type' => 'sectionend', 'id' => 'account_password_options' );
     return $settings;
 }
 add_filter('woocommerce_min_password_strength','ds_change_password_strength',30);
-function ds_change_password_strength(){
+function ds_change_password_strength() {
     $strength=get_option( 'woocommerce_myaccount_password_strength', null );
     return intval($strength);
     
+}
+add_action( 'wp_enqueue_scripts',  'ds_load_scripts',99 );
+function ds_load_scripts() {
+    wp_localize_script( 'wc-password-strength-meter', 'pwsL10n', array(
+        'empty' => __( get_option( 'woocommerce_password_strength_label_1', null ) ),
+        'short' => __( get_option( 'woocommerce_password_strength_label_2', null ) ),
+        'bad' => __( get_option( 'woocommerce_password_strength_label_3', null ) ),
+        'good' => __( get_option( 'woocommerce_password_strength_label_4', null ) ),
+        'strong' => __( get_option( 'woocommerce_password_strength_label_5', null ) ),
+        'mismatch' => __( 'Your passwords do not match, please re-enter them.' )
+    ) );
+}
+
+add_filter( 'wc_password_strength_meter_params', 'ds_strength_meter_custom_strings' );
+function ds_strength_meter_custom_strings( $data ) {
+    $data_new = array(
+        'i18n_password_error'   => esc_attr__( get_option( 'woocommerce_password_error', null ), 'woocommerce' ),
+    );
+
+    return array_merge( $data, $data_new );
 }
